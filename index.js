e4fb42a9396671bca57595837fc400fe647121fb
Michael <michaellimisi7@gmail.com>
2023-03-14 03:26:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+let arr = [];
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+let label ="Cards";
+
+for (let i = 0; i < cards.length; i++) {
+    let obj = {};
+    obj['data'] = `Thank you, ${cards[i]},for the wonderful suprise gift!`;
+    arr.push(obj['data']);
+    debugger;
+}
+console.log(arr);
+
+// 
+for (let x = 10; x >= 0; x--){
+    console.log(x);
+    debugger;
+}
